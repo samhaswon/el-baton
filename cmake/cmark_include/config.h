7b0a2053cb884a702a6c31e3f39dd2b883729f41
samhaswon <sahoward42@tntech.edu>
2026-07-28 21:23:22 -0500

    Fix MSVC error

--- cmake/cmark_include/config.h
+++ cmake/cmark_include/config.h
@@ -1,14 +1,21 @@
 #pragma once
 
 #define HAVE_STDBOOL_H 1
-#define HAVE___BUILTIN_EXPECT 1
-#define HAVE___ATTRIBUTE__ 1
 
 #include <stdbool.h>
 
+#if defined(__clang__) || defined(__GNUC__)
+#define HAVE___BUILTIN_EXPECT 1
+#define HAVE___ATTRIBUTE__ 1
 #define CMARK_ATTRIBUTE(list) __attribute__(list)
-
-#ifndef CMARK_INLINE
-#define CMARK_INLINE inline
+#else
+#define CMARK_ATTRIBUTE(list)
 #endif
 
+#ifndef CMARK_INLINE
+#if defined(_MSC_VER) && !defined(__cplusplus)
+#define CMARK_INLINE __inline
+#else
+#define CMARK_INLINE inline
+#endif
+#endif
